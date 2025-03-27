857bbe7565311c93182628ad85d97817357b3dc0
SrihariSakshith <kotichintalasriharisakshith@gmail.com>
2025-03-28 01:54:43 +0530

    updated code

--- server.js
+++ server.js
@@ -95,3 +95,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Export the app object for testing or external use
+export default app;
